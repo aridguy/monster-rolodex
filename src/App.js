50a08ba4f0189709aae2b9d2444c261852b03ab1
aridguy <rotimiariyo@gmail.com>
2022-09-24 18:02:15 +0100

    moster rolodex class react class

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+      this.state = {
+       Monstar1:{
+            name: 'Ariyo'
+       },
+
+       Monstar2: {
+        name: 'Taiwo'
+       },
+
+       monster3:{
+        name: 'damilare'
+       },
+      
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+
+}
+
+export default App;
